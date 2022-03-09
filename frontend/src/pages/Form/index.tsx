fec29e1c28b4735d3bd0ace044055b15e6a92a79
Avelardo <avelar.vignoto@gmail.com>
2022-03-09 12:35:53 -0300

    link das páginas

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './style.css'; 
 
 function Form() {
@@ -13,8 +14,10 @@ function Form() {
     return (
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
+
                 <form className="dsmovie-form">
                     <div className="form-group dsmovie-form-group">
                         <label htmlFor="email">Informe seu email</label>
@@ -34,7 +37,11 @@ function Form() {
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
+
             </div >
         </div >
     )
