360834f644606ff3ce68efc656f6e7fe5abcfe6b
Avelardo <avelar.vignoto@gmail.com>
2022-03-12 10:59:49 -0300

    params

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -4,6 +4,7 @@ import { useState} from 'react';
 import {useEffect } from 'react';
 import axios from 'axios';
 import {MoviePage} from 'types/movie';
+import { BASE_URL } from 'utils/request';
 
 function Listing() {
 
